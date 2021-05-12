703ca8e942dc29ff25f5488eb2c261ff20e42a73
Pedro Arturo Higuera Garzón <higueragarzonpedroarturo@gmail.com>
2021-05-12 13:59:43 -0500

    Se agregaron nuevos modos de uso, para wifi y para whatsapp

--- scripts/generator.js
+++ scripts/generator.js
@@ -2,9 +2,22 @@ let qrdiv = select('qrcode')
 let qrlink = select('qr_input')
 let qrbutton = select('qr_button')
 let qrdownload = select('qr_download')
+let urlmode = select('url_mode')
+let wifimode = select('wifi_mode')
+let wpmode = select('wp_mode')
+let name_network = select('name_network')
+let security_network = select('security_network')
+let pass_network = select('pass_network')
+let indicativo = select('indicativo')
+let number_phone = select('number_phone')
+
+var mode =1;
 
 qrbutton.addEventListener('click',generateQR)
 qrdownload.addEventListener('click',downloadQR)
+urlmode.addEventListener('click',urlMode)
+wifimode.addEventListener('click',wifiMode)
+wpmode.addEventListener('click',wpMode)
 
 var qrcode = new QRCode(qrdiv,{
     text: "",
@@ -16,7 +29,14 @@ var qrcode = new QRCode(qrdiv,{
 })
 
 function generateQR(){
+	if(mode==1){
     qrcode.makeCode(qrlink.value)
+	}else if(mode==2){
+		let wifiURL ='WIFI:S:'+name_network.value+';T:'+security_network.value+';P:'+pass_network.value+';H:True;;'
+		qrcode.makeCode(wifiURL)
+	}else{
+		qrcode.makeCode('https://wa.me/'+indicativo.value+number_phone.value)
+	}
 }
 
 function downloadQR(){
@@ -28,6 +48,46 @@ function downloadQR(){
 		// Hacer click en él
 		enlace.click();
 	});
+}
+
+function urlMode(){
+	document.getElementById('url_p').style.display='block'
+	qrlink.style.display='block'
+	document.getElementById('wifi_name').style.display='none'
+	document.getElementById('wifi_pass').style.display='none'
+	document.getElementById('wifi_sec').style.display='none'
+	document.getElementById('name_network').style.display='none'
+	document.getElementById('pass_network').style.display='none'
+	document.getElementById('security_network').style.display='none'
+	document.getElementById('wp_p').style.display='none'
+	document.getElementById('number_div').style.display='none'
+	mode=1
+}
+function wifiMode(){
+	document.getElementById('wifi_name').style.display='block'
+	document.getElementById('wifi_pass').style.display='block'
+	document.getElementById('wifi_sec').style.display='block'
+	document.getElementById('name_network').style.display='block'
+	document.getElementById('pass_network').style.display='block'
+	document.getElementById('security_network').style.display='block'
+	document.getElementById('url_p').style.display='none'
+	document.getElementById('wp_p').style.display='none'
+	document.getElementById('number_div').style.display='none'
+	qrlink.style.display='none'
+	mode=2
+}
+function wpMode(){
+	document.getElementById('number_div').style.display='block'
+	document.getElementById('wp_p').style.display='block'
+	document.getElementById('wifi_name').style.display='none'
+	document.getElementById('wifi_pass').style.display='none'
+	document.getElementById('wifi_sec').style.display='none'
+	document.getElementById('name_network').style.display='none'
+	document.getElementById('pass_network').style.display='none'
+	document.getElementById('security_network').style.display='none'
+	document.getElementById('url_p').style.display='none'
+	qrlink.style.display='none'
+	mode=3
 }
 
 function select(valor){
